f7b43a28c09a42169792f29fa799cbdab6cea403
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2023-12-03 11:41:42 -0800

    Start get, addFirst, addLast, toList tests and methods

--- proj1b/src/ArrayDeque.java
+++ proj1b/src/ArrayDeque.java
@@ -1,41 +1,165 @@
+import java.util.ArrayList;
 import java.util.List;
 
+// index:   0 1 2 3 4 5 6 7
+// items: [-9 1 3 4 0 0 0 0]
+// size: 8
+
+/**
+ * Implementation of a deque using an array with a circular structure.
+ *
+ * @param <T> The type of elements stored in the deque.
+ */
 public class ArrayDeque<T> implements Deque<T> {
+    // Instance variables
+
+    /**
+     * The actual elements of type T.
+     */
+    private T[] items;
+
+    /**
+     * The number of elements in the array deque.
+     */
+    private int size;
+
+    /**
+     * Index of the first array element.
+     */
+    private int nextFirst;
+
+    /**
+     * Index of the last array element.
+     */
+    private int nextLast;
+
+    private static final int INITIAL_CAPACITY = 8;
+
 
     /** Constructor */
     public ArrayDeque() {
+        // Initialize array with initial size of 8
+        items = (T[]) new Object[INITIAL_CAPACITY];
+
+        // Initialize nextFirst and nextLast circular references.
+        nextFirst = 0; // First element goes to index 0
+        nextLast = 0; // Next "last" element added to index 1
+
+        // Initialize size to 0 as array deque has no elements.
+        size = 0;
 
     }
 
     /**
-     * Add {@code x} to the front of the deque. Assumes {@code x} is never null.
+     * Resizes the backing array to the target capacity.
+     * TODO: WORK ON RESIZING AFTER ALL METHODS WORK WITH FIXED-SIZE ARRAY FIRST.
+     */
+//    private void resize(int capacity) {
+//        int[] a = (T[]) new Object(capacity);
+//    }
+
+    /**
+     * Add an item to the front of the deque. Assumes the item is never null.
      *
      * @param x item to add
      */
     @Override
     public void addFirst(T x) {
+        // Calculate the index to add the new item
+        int newIndex = (nextFirst - 1 + items.length) % items.length;
 
+        // Add the new item to the calculated index
+        items[newIndex] = x;
+
+        // Adjust circular references
+        nextFirst = newIndex;
+
+        // Increment size, the number of elements in the deque (NOT the capacity or index)
+        size += 1;
     }
 
     /**
-     * Add {@code x} to the back of the deque. Assumes {@code x} is never null.
+     * Add an item to the back of the deque. Assumes item is never null.
      *
      * @param x item to add
      */
     @Override
     public void addLast(T x) {
+        // Calculate index to add the new item
+        int newIndex = nextLast;
 
+        // Add the new item to the calculated index
+        items[newIndex] = x;
+
+        // Adjust the references
+        nextLast = (newIndex + 1) % items.length;
+
+        // Increment size, the number of elements in the deque (NOT the capacity or index)
+        size += 1;
     }
 
     /**
-     * Returns a List copy of the deque. Does not alter the deque.
+     * Returns a List copy of the deque, so we can verify its values. Does not alter the deque.
      *
      * @return a new list copy of the deque.
      */
+
     @Override
     public List<T> toList() {
-        return null;
+        List<T> returnList = new ArrayList<>();  // TODO: pass in size, the number of actual elements? or capacity if capacity == items.length?
+
+        // Start where?? at nextFirst, the index of first element as defined
+        int startIndex = nextFirst;
+
+        // Iterate through all elements after the nextFirst
+        while (size != 0) {
+            returnList.add(items[startIndex]);
+            // Move to next index...in a loop...
+            startIndex = (startIndex + 1) % items.length;
+            size -= 1;
+        }
+        return returnList;
     }
+
+//    @Override
+//    public List<T> toListWithNulls() {
+//        List<T> returnList = new ArrayList<>();
+//
+//        // Start where?? at nextFirst, the index of first element as defined
+//        int current = nextFirst;
+//
+//        // Iterate through all index up to capacity
+//        while (size != items.length) {
+//            if (items[current] == null) {
+//                returnList.add(null);
+//            } else {
+//                returnList.add(items[current]);
+//            }
+//            // Move to next index...in a loop...
+//            current = (current + 1) % items.length;
+//            size -= 1;
+//        }
+//        return returnList;
+//    }
+//
+//    public List<T> toListUsingForLoopWithNulls() {
+//        List<T> returnList = new ArrayList<>();
+//
+//        for (int i = 0; i < items.length; i += 1) {
+    /**
+     * Start at nextFirst and traverse the length of the index (each index--not size, which is about elements) and go back to nextFirst
+     */
+//            int index = (nextFirst + i) % items.length;
+//            if (items[index] == null) {
+//                returnList.add(null);
+//            } else {
+//                returnList.add(items[index]);
+//            }
+//        }
+//
+//        return returnList;
+//    }
+
 
     /**
      * Returns if the deque is empty. Does not alter the deque.
--- proj1b/tests/ArrayDequeTest.java
+++ proj1b/tests/ArrayDequeTest.java
@@ -1,23 +1,180 @@
 import jh61b.utils.Reflection;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
+import java.sql.Array;
 import java.util.List;
 
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDequeTest {
+    @Test
+    @DisplayName("ArrayDeque has no fields besides backing array and primitives")
+    void noNonTrivialFields() {
+        List<Field> badFields = Reflection.getFields(ArrayDeque.class)
+                .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
+                .toList();
 
-    // @Test
-    // @DisplayName("ArrayDeque has no fields besides backing array and primitives")
-    // void noNonTrivialFields() {
-    //     List<Field> badFields = Reflection.getFields(ArrayDeque.class)
-    //             .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
-    //             .toList();
+        assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
+    }
 
-    //     assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
-    // }
+    @Test
+    @DisplayName("Check that toList works with empty array deque")
+    void testToListEmpty() {
+        // Arrange
+        Deque<Integer> ad1 = new ArrayDeque<>();
+
+        // Assert
+        assertThat(ad1.toList()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Check that toList works with NON-empty array deque")
+    void testToListNonEmpty() {
+        // Arrange
+        Deque<Integer> ad1 = new ArrayDeque<>();
+
+        // Act: add elements
+
+        ad1.addFirst(7); // expect: [7]
+        ad1.addFirst(6); // expect: [6, 7]
+        ad1.addFirst(5); // expect: [5, 6, 7]
+        ad1.addFirst(4); // expect: [4, 5, 6 ,7]
+        ad1.addFirst(3); // expect: [3, 4, 5, 6, 7]
+        ad1.addFirst(2); // expect: [2, 3, 4, 5, 6, 7]
+        ad1.addFirst(1); // expect: [1, 2, 3, 4, 5, 6, 7]
+        ad1.addLast(8); // expect: [1, 2, 3, 4, 5, 6, 7, 8]
+
+        // Assert
+        assertThat(ad1.toList()).containsExactly(1, 2, 3, 4, 5, 6, 7, 8).inOrder();
+    }
+
+
+    @Test
+    @DisplayName("Check that addFirst works on empty deque")
+    void testAddFirstFromEmpty() {
+         // Arrange: create empty array deque
+        Deque<String> ad1 = new ArrayDeque<>();
+
+        // Act: add elements
+        ad1.addFirst("back"); // expect: [back]
+        ad1.addFirst("middle"); // expect: [middle, back]
+        ad1.addFirst("front"); // expect: [front, middle, back]
+
+        // Assert
+        assertThat(ad1.toList()).containsExactly("front", "middle", "back").inOrder();
+    }
+    @Test
+    @DisplayName("Check that addFirst works on non-empty deque")
+    void testAddFirstFromNonEmpty() {
+        // Arrange and Act: create an array deque and add initial items
+        Deque<String> ad1 = new ArrayDeque<>();
+        ad1.addFirst("back"); // expect: [back]
+        ad1.addFirst("middle"); // expect: [middle, back]
+
+        // Act: add an element to front
+        ad1.addFirst("front"); // expect: [front, middle, back]
+
+        // Assert
+        assertThat(ad1.toList()).containsExactly("front", "middle", "back").inOrder();
+    }
+    @Test
+    @DisplayName("Check that addLast works on empty deque")
+    void testAddLastFromEmpty() {
+        // Arrange: create empty array deque
+        Deque<String> ad1 = new ArrayDeque<>();
+
+        // Act: add elements
+        ad1.addLast("front"); // expect: [front]
+        ad1.addLast("middle"); // expect: [front, middle]
+        ad1.addLast("back"); // expect: [front, middle, back]
+
+        // Assert
+        assertThat(ad1.toList()).containsExactly("front", "middle", "back").inOrder();
+    }
+    @Test
+    @DisplayName("Check that addLast works on NON empty deque")
+    void testAddLastFromNonEmpty() {
+        // Arrange and Act: create an array deque and add initial items
+        Deque<String> ad1 = new ArrayDeque<>();
+        ad1.addLast("front"); // expect: [front]
+        ad1.addLast("middle"); // expect: [front, middle]
+
+        // Act: add an element to front
+        ad1.addLast("back"); // expect: [front, middle, back]
+
+        // Assert
+        assertThat(ad1.toList()).containsExactly("front", "middle", "back").inOrder();
+    }
+
+    @Test
+    @DisplayName("Check that get works on an empty deque")
+    void testGetEmptyDeque() {
+         // Arrange: create an empty deque
+        Deque<Character> ad1 = new ArrayDeque<>();
+
+        //Assert: check that get with any index returns null in empty deque
+        assertThat(ad1.get(-1)).isNull();
+        assertThat(ad1.get(0)).isNull();
+        assertThat(ad1.get(7)).isNull();
+    }
+
+    @Test
+    @DisplayName("Check that get works on a valid index")
+    void testGetValidIndex() {
+        // Arrange: create an empty deque
+        Deque<Character> ad1 = new ArrayDeque<>();
+
+        // Act: add elements
+        ad1.addFirst('A'); // expect: [A]
+        ad1.addLast('D'); // expect: [A, D]
+        ad1.addLast('e'); // expect: [A, D, e]
+        ad1.addFirst('z'); // expect: [z, A, D, e]
+        ad1.addLast('B'); // expect: [z, A, D, e, B]
+
+        // Assert
+        assertThat(ad1.get(0)).isEqualTo('z');
+        assertThat(ad1.get(2)).isEqualTo('D');
+        assertThat(ad1.get(4)).isEqualTo('B');
+
+    }
+    @Test
+    @DisplayName("Check that get works on a large, out of bounds index")
+    void testGetOutOfBoundsIndex() {
+        // Arrange: create an empty deque
+        Deque<Character> ad1 = new ArrayDeque<>();
+
+        // Act: add elements
+        ad1.addFirst('A'); // expect: [A]
+        ad1.addLast('D'); // expect: [A, D]
+        ad1.addLast('e'); // expect: [A, D, e]
+        ad1.addFirst('z'); // expect: [z, A, D, e]
+        ad1.addLast('B'); // expect: [z, A, D, e, B]
+
+        // Assert
+        assertThat(ad1.get(23930234)).isNull();
+    }
+
+    @Test
+    @DisplayName("Check that get works on negative index")
+    void testGetNegativeIndex() {
+        // Arrange: create an empty deque
+        Deque<Character> ad1 = new ArrayDeque<>();
+
+        // Act: add elements
+        ad1.addFirst('A'); // expect: [A]
+        ad1.addLast('D'); // expect: [A, D]
+        ad1.addLast('e'); // expect: [A, D, e]
+        ad1.addFirst('z'); // expect: [z, A, D, e]
+        ad1.addLast('B'); // expect: [z, A, D, e, B]
+
+        // Assert
+        assertThat(ad1.get(-1)).isNull();
+        assertThat(ad1.get(-244343)).isNull();
+
+    }
 
 }

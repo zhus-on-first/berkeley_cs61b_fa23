b01b15bc109058753a66d9e3f8a89a119813b62a
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2023-12-12 15:13:55 -0800

    Complete buffer constructor

--- proj1c/src/gh2/GuitarString.java
+++ proj1c/src/gh2/GuitarString.java
@@ -1,5 +1,6 @@
 package gh2;
 
+import deque.ArrayDeque;
 import deque.Deque;
 // TODO: maybe more imports
 
@@ -13,7 +14,7 @@ public class GuitarString {
 
     /* Buffer for storing sound data. */
     // TODO: uncomment the following line once you're ready to start this portion
-    // private Deque<Double> buffer;
+     private Deque<Double> buffer;
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
@@ -21,8 +22,16 @@ public class GuitarString {
         //       cast the result of this division operation into an int. For
         //       better accuracy, use the Math.round() function before casting.
         //       Your should initially fill your buffer array with zeros.
-    }
 
+        // Calculate buffer capacity
+        int capacity = (int) Math.round(SR / frequency);
+
+        // Initialize buffer with zeros
+        buffer = new ArrayDeque<>();
+        for (int i = 0; i < capacity; i++) {
+            buffer.addFirst((double) 0);
+        }
+    }
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
@@ -34,6 +43,8 @@ public class GuitarString {
         //       other. This does not mean that you need to check that the numbers
         //       are different from each other. It means you should repeatedly call
         //       Math.random() - 0.5 to generate new random numbers for each array index.
+
+
     }
 
     /* Advance the simulation one time step by performing one iteration of

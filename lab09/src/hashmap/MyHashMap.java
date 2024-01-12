5e2c86f2ff9018f8bed025e4e30d02ca6b5ceb6c
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2024-01-11 16:35:46 -0800

    Setup implementation skeletal code

--- lab09/src/hashmap/MyHashMap.java
+++ lab09/src/hashmap/MyHashMap.java
@@ -1,12 +1,14 @@
 package hashmap;
 
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  *  A hash table-backed Map implementation.
  *
  *  Assumes null keys will never be inserted, and does not resize down upon remove().
- *  @author YOUR NAME HERE
+ *  @author Jia-Jia Zhu
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
 
@@ -66,5 +68,101 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
 
     // TODO: Implement the methods of the Map61B Interface below
     // Your code won't compile until you do so!
+
+    /**
+     * Associates the specified value with the specified key in this map.
+     * If the map already contains the specified key, replaces the key's mapping
+     * with the value specified.
+     *
+     * @param key
+     * @param value
+     */
+    @Override
+    public void put(K key, V value) {
+        // hash(key) -> index corresponding to a bucket to place it in
+        // get bucket by indexing into the buckets array
+        // if bucket contains given key, update key's value
+        // else, add a new Node<K, V> to bucket's collection
+
+        // track size and when to resize
+
+    }
+
+    /**
+     * Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     *
+     * @param key
+     */
+    @Override
+    public V get(K key) {
+        // hash(key) -> index corresponding to a bucket to place it in
+        // get bucket by indexing into the buckets array
+        // if bucket contains given key, return key's value
+        // else, return null
+        return null;
+    }
+
+    /**
+     * Returns whether this map contains a mapping for the specified key.
+     *
+     * @param key
+     */
+    @Override
+    public boolean containsKey(K key) {
+        // hash(key) -> index corresponding to a bucket to place it in
+        // get bucket by indexing into the buckets array
+        // if bucket contains given key, return true
+        // else, return false
+        return false;
+    }
+
+    /**
+     * Returns the number of key-value mappings in this map.
+     */
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    /**
+     * Removes every mapping from this map.
+     */
+    @Override
+    public void clear() {
+
+    }
+
+    /**
+     * Returns a Set view of the keys contained in this map. Not required for Lab 9.
+     * If you don't implement this, throw an UnsupportedOperationException.
+     */
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException("Not implementing");
+    }
+
+    /**
+     * Removes the mapping for the specified key from this map if present,
+     * or null if there is no such mapping.
+     * Not required for Lab 9. If you don't implement this, throw an
+     * UnsupportedOperationException.
+     *
+     * @param key
+     */
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException("Not implementing");
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException("Not implementing");
+    }
 
 }
